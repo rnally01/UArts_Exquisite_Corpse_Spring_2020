61a28734b5c39494599d8acf663488a0271d4b9b
mgoliveri6 <62576670+mgoliveri6@users.noreply.github.com>
2020-05-10 18:34:25 -0400

    Moises Commit
    walls with textures, terrain texture, ceiling texture, new audio, enemies, and spawning

---
+++ Blank_Unity_Project/Assets/Scripts/Enemies.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemies : MonoBehaviour
+{
+    public Transform player;
+    private NavMeshAgent agent;
+    private Vector3 agentHome;
+    private Vector3 destination;
+    private float distanceBetween;
+    public float minimumChaseDistance = 4f;
+    public float maximumChaseDistance = 25f;
+
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        destination = agent.destination;
+
+        agentHome = agent.transform.position;
+    }
+
+    private void Update()
+    {
+        distanceBetween = Vector3.Distance(agent.transform.position, player.position);
+
+        //Debug.Log(distanceBetween);
+
+        if (distanceBetween > minimumChaseDistance && distanceBetween < maximumChaseDistance)
+        {
+            destination = player.position;
+            agent.destination = destination;
+            //gameObject.transform.LookAt(player);
+        }
+
+        if (distanceBetween < minimumChaseDistance)
+        {
+            //Debug.Log("Shut up, too close");
+        }
+
+        if (distanceBetween > maximumChaseDistance)
+        {
+            //Debug.Log("too far");
+            destination = agentHome;
+            agent.destination = destination;
+        }
+    }
+}
---
+++ Blank_Unity_Project/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour
+{
+
+    public Transform spawnPoint;
+    public Transform spawnObject;
+    private int spawnTotal = 10;
+    public float timeBetweenSpawns;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(SpawnGameObject());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator SpawnGameObject()
+    {
+        for (var x = 0; x < spawnTotal; x++)
+        {
+            Instantiate(spawnObject, spawnPoint.position, spawnPoint.rotation);
+            yield return new WaitForSeconds(timeBetweenSpawns);
+        }
+    }
+}
